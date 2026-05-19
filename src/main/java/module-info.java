292a1bc2125aa3d6863b93c74fd193d341574b27
ImReallyNotInfected <vinhkhangfreefire@gmail.com>
2026-05-19 18:54:48 +0700

    AA

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.yourlibrary.core {
-    requires net.minestom.server;
-    requires org.mongodb.driver.sync.client;
-    requires org.mongodb.driver.core;
-    requires org.mongodb.bson;
-
-    exports org.imnotasecurity.api;
-}
